a7a26bdbfc3644173075efcd51eacd81a6aa8dff
Filip Markelic <c3304486@uon.edu.au>
2019-04-29 00:50:21 +0000

    Somehow made this to work through guidance of youtube

--- main.c
+++ main.c
@@ -1,11 +1,68 @@
 #include <stdio.h>
+#include <string.h>
+
+void Encrypt (char msg [], int key);
+
+void Decrypt (char *p, int key);
 
 int main() {
-    asdf
-    asdf
-    asdf
-    asdf
-    doing
-    research
-  return 0;
+  int key;
+  char str [50];
+  printf ("");
+  gets (str);
+  printf ("");
+  scanf ("%d", &key);
+  
+  if (key > 0 && key < 26) // as per requirements
+  {
+      Encrypt (str, key);
+      printf ("Encrypted message as: %s\n", str);
+      
+      Decrypt (str, key);
+      printf ("Decrypted message as: %s", str);
+      
+  }
+  else
+  printf ("\n Key is invalid");
+}
+
+void Encrypt (char message[], int key){
+    int i, ch;
+    int change, temp, key_temp;
+    
+    for (i = 0, message [i] != '\0'; ++i;)
+    {
+            ch = message [i];
+            if ((ch >= 65 && ch <= 90))
+        {
+            change = ch + key;
+            if (change <= 90)
+            {
+                ch = ch + key;
+            }
+            else
+            {
+                temp = 90 - ch;
+                key_temp = key - temp;
+                ch = 64 + key_temp;
+            }
+            message[i] = ch;
+    }
+    else
+    if((ch >= 97 && ch <= 122))
+    {
+        change = ch + key;
+        if (change <= 122)
+        {
+            ch = ch + key;
+        }
+        else
+        {
+            temp= 122 - ch;
+            key_temp = key - temp;
+            ch = 96 + key_temp;
+        }
+        message [i] = ch;
+}
+}
 }
